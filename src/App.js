7560496f972f0b2c0d1bf3e5e3be1afdc814bf6f
Anikroy7 <roy645800@gmail.com>
2022-04-06 18:36:54 +0600

    handel google sign in with pop up

--- src/App.js
+++ src/App.js
@@ -1,14 +1,26 @@
-import { getAuth } from "firebase/auth";
+import { getAuth, GoogleAuthProvider, signInWithPopup } from "firebase/auth";
 import './App.css';
 import app from './firebase.init'
 const auth = getAuth(app)
 
+const provider = new GoogleAuthProvider();
+
+const handelGoogleSignIn = () => {
+
+  signInWithPopup(auth, provider)
+    .then(result => {
+      console.log(result.user)
+    })
+    .catch(error => {
+      console.error('error massage: ', error)
+    })
+}
 
 function App() {
   return (
     <div className="App">
       <h1>Sing in with Google</h1>
-      <button>google sign in</button>
+      <button onClick={handelGoogleSignIn}>google sign in</button>
     </div>
   );
 }
