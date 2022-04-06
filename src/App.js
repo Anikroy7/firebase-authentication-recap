8b57b1c474fd1f2440a1def3a192182f5ebbf4e1
Anikroy7 <roy645800@gmail.com>
2022-04-06 18:17:09 +0600

    page set up for authentication with react update version

--- src/App.js
+++ src/App.js
@@ -1,23 +1,11 @@
-import logo from './logo.svg';
+
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Sing in with Google</h1>
+      <button>google sign in</button>
     </div>
   );
 }
