65cb3a8643e462ef144ac74c91a8ed8d6d887d6a
Anikroy7 <roy645800@gmail.com>
2022-04-06 18:59:47 +0600

    display user information in ui

--- src/App.js
+++ src/App.js
@@ -1,26 +1,37 @@
+import { getByDisplayValue } from "@testing-library/react";
 import { getAuth, GoogleAuthProvider, signInWithPopup } from "firebase/auth";
+import { useState } from "react";
 import './App.css';
 import app from './firebase.init'
 const auth = getAuth(app)
 
-const provider = new GoogleAuthProvider();
 
-const handelGoogleSignIn = () => {
 
-  signInWithPopup(auth, provider)
-    .then(result => {
-      console.log(result.user)
-    })
-    .catch(error => {
-      console.error('error massage: ', error)
-    })
-}
 
 function App() {
+
+  const provider = new GoogleAuthProvider();
+
+  const [user, setUser] = useState({});
+
+  const handelGoogleSignIn = () => {
+
+    signInWithPopup(auth, provider)
+      .then(result => {
+        console.log(result.user);
+        setUser(result.user)
+      })
+      .catch(error => {
+        console.error('error massage: ', error)
+      })
+  }
   return (
     <div className="App">
       <h1>Sing in with Google</h1>
       <button onClick={handelGoogleSignIn}>google sign in</button>
+      <h3>Name: {user.displayName}</h3>
+      <h3>Email: {user.email}</h3>
+      <img src={user.photoURL} alt="" />
     </div>
   );
 }
